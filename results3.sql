44adb3153256a8a2f61c0f0950666bc0e4319e94
Артем Зотов <178171242+AllayBro@users.noreply.github.com>
2025-05-18 05:24:04 +0300

    Add files via upload

---
+++ results3.sql
@@ -0,0 +1,104 @@
+                                                title                                                 | ratingcount | avgrating 
+------------------------------------------------------------------------------------------------------+-------------+-----------
+ Shawshank Redemption, The (1994)                                                                     |       63366 |      4.45
+ Godfather, The (1972)                                                                                |       41355 |      4.36
+ Usual Suspects, The (1995)                                                                           |       47006 |      4.33
+ Schindler's List (1993)                                                                              |       50054 |      4.31
+ Godfather: Part II, The (1974)                                                                       |       27398 |      4.28
+ Rear Window (1954)                                                                                   |       17449 |      4.27
+ Seven Samurai (Shichinin no samurai) (1954)                                                          |       11611 |      4.27
+ Sunset Blvd. (a.k.a. Sunset Boulevard) (1950)                                                        |        6525 |      4.26
+ Casablanca (1942)                                                                                    |       24349 |      4.26
+ Band of Brothers (2001)                                                                              |        4305 |      4.26
+ Third Man, The (1949)                                                                                |        6565 |      4.25
+ Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb (1964)                          |       23220 |      4.25
+ One Flew Over the Cuckoo's Nest (1975)                                                               |       29932 |      4.25
+ City of God (Cidade de Deus) (2002)                                                                  |       12937 |      4.24
+ North by Northwest (1959)                                                                            |       15627 |      4.23
+ Fight Club (1999)                                                                                    |       40106 |      4.23
+ Paths of Glory (1957)                                                                                |        3568 |      4.23
+ Lives of Others, The (Das leben der Anderen) (2006)                                                  |        5720 |      4.23
+ Dark Knight, The (2008)                                                                              |       20438 |      4.22
+ 12 Angry Men (1957)                                                                                  |       12934 |      4.22
+ Raiders of the Lost Ark (Indiana Jones and the Raiders of the Lost Ark) (1981)                       |       43295 |      4.22
+ Double Indemnity (1944)                                                                              |        4909 |      4.22
+ Cosmos (1980)                                                                                        |         936 |      4.22
+ Yojimbo (1961)                                                                                       |        3559 |      4.21
+ Big Sleep, The (1946)                                                                                |        5529 |      4.21
+ Notorious (1946)                                                                                     |        4932 |      4.20
+ All About Eve (1950)                                                                                 |        4826 |      4.20
+ Spirited Away (Sen to Chihiro no kamikakushi) (2001)                                                 |       13466 |      4.20
+ Amelie (Fabuleux destin d'AmÃ©lie Poulain, Le) (2001)                                                |       24349 |      4.20
+ Chinatown (1974)                                                                                     |       15310 |      4.20
+ Maltese Falcon, The (1941)                                                                           |       12144 |      4.19
+ Star Wars: Episode IV - A New Hope (1977)                                                            |       54502 |      4.19
+ M (1931)                                                                                             |        4232 |      4.19
+ To Kill a Mockingbird (1962)                                                                         |       14769 |      4.19
+ Matrix, The (1999)                                                                                   |       51334 |      4.19
+ Star Wars: Episode V - The Empire Strikes Back (1980)                                                |       45313 |      4.19
+ Princess Bride, The (1987)                                                                           |       32586 |      4.18
+ Thin Man, The (1934)                                                                                 |        3358 |      4.18
+ Rashomon (RashÃ´mon) (1950)                                                                          |        3712 |      4.18
+ Memento (2000)                                                                                       |       30443 |      4.18
+ Silence of the Lambs, The (1991)                                                                     |       63299 |      4.18
+ Black Mirror (2011)                                                                                  |         582 |      4.18
+ Life Is Beautiful (La Vita Ã¨ bella) (1997)                                                          |       18156 |      4.18
+ Wallace & Gromit: The Wrong Trousers (1993)                                                          |       15022 |      4.18
+ Touch of Evil (1958)                                                                                 |        4718 |      4.18
+ Goodfellas (1990)                                                                                    |       26406 |      4.18
+ Pulp Fiction (1994)                                                                                  |       67310 |      4.17
+ Decalogue, The (Dekalog) (1989)                                                                      |         402 |      4.17
+ Sting, The (1973)                                                                                    |       14926 |      4.17
+ Wallace & Gromit: A Close Shave (1995)                                                               |       12073 |      4.17
+ Ran (1985)                                                                                           |        4824 |      4.17
+ Philadelphia Story, The (1940)                                                                       |        6583 |      4.17
+ Monty Python and the Holy Grail (1975)                                                               |       33024 |      4.17
+ City Lights (1931)                                                                                   |        2593 |      4.17
+ It Happened One Night (1934)                                                                         |        4184 |      4.17
+ Strangers on a Train (1951)                                                                          |        5154 |      4.16
+ General, The (1926)                                                                                  |        2224 |      4.16
+ Lawrence of Arabia (1962)                                                                            |       12378 |      4.16
+ Inception (2010)                                                                                     |       14023 |      4.16
+ American Beauty (1999)                                                                               |       44987 |      4.16
+ When We Were Kings (1996)                                                                            |        3737 |      4.16
+ Shadow of a Doubt (1943)                                                                             |        2199 |      4.16
+ Treasure of the Sierra Madre, The (1948)                                                             |        5155 |      4.16
+ On the Waterfront (1954)                                                                             |        5644 |      4.16
+ Cinema Paradiso (Nuovo cinema Paradiso) (1989)                                                       |        9100 |      4.15
+ Witness for the Prosecution (1957)                                                                   |        1620 |      4.15
+ His Girl Friday (1940)                                                                               |        3902 |      4.15
+ Grand Illusion (La grande illusion) (1937)                                                           |        1594 |      4.15
+ American History X (1998)                                                                            |       23309 |      4.15
+ My Neighbor Totoro (Tonari no Totoro) (1988)                                                         |        5489 |      4.15
+ Vertigo (1958)                                                                                       |       14094 |      4.15
+ Song of the Little Road (Pather Panchali) (1955)                                                     |         761 |      4.14
+ Great Escape, The (1963)                                                                             |       10904 |      4.14
+ Bicycle Thieves (a.k.a. The Bicycle Thief) (a.k.a. The Bicycle Thieves) (Ladri di biciclette) (1948) |        3186 |      4.14
+ 400 Blows, The (Les quatre cents coups) (1959)                                                       |        2851 |      4.14
+ Nights of Cabiria (Notti di Cabiria, Le) (1957)                                                      |        1152 |      4.14
+ Raise the Red Lantern (Da hong deng long gao gao gua) (1991)                                         |        3253 |      4.14
+ Boot, Das (Boat, The) (1981)                                                                         |       13612 |      4.14
+ Jean de Florette (1986)                                                                              |        3247 |      4.14
+ Modern Times (1936)                                                                                  |        3732 |      4.14
+ Lord of the Rings: The Fellowship of the Ring, The (2001)                                            |       37553 |      4.14
+ Lord of the Rings: The Return of the King, The (2003)                                                |       31577 |      4.14
+ Blade Runner (1982)                                                                                  |       30526 |      4.13
+ Citizen Kane (1941)                                                                                  |       17774 |      4.13
+ Man for All Seasons, A (1966)                                                                        |        2123 |      4.13
+ Lady Eve, The (1941)                                                                                 |        1398 |      4.13
+ Fawlty Towers (1975-1979)                                                                            |         230 |      4.13
+ Once Upon a Time in the West (C'era una volta il West) (1968)                                        |        4407 |      4.13
+ Duck Soup (1933)                                                                                     |        5569 |      4.13
+ Thin Blue Line, The (1988)                                                                           |        2700 |      4.13
+ Stop Making Sense (1984)                                                                             |        2192 |      4.13
+ Manchurian Candidate, The (1962)                                                                     |        8988 |      4.13
+ Intouchables (2011)                                                                                  |        2738 |      4.13
+ World of Apu, The (Apur Sansar) (1959)                                                               |         765 |      4.12
+ Good, the Bad and the Ugly, The (Buono, il brutto, il cattivo, Il) (1966)                            |       14496 |      4.12
+ Rebecca (1940)                                                                                       |        4652 |      4.12
+ Hustler, The (1961)                                                                                  |        3955 |      4.12
+ Creature Comforts (1989)                                                                             |        2585 |      4.12
+ Henry V (1989)                                                                                       |        4656 |      4.12
+ Ikiru (1952)                                                                                         |        1118 |      4.12
+(100 ñòðîê)
+
